fcc2e1efd1a97af89ba09ab5da9b0b6f1774755d
dmclaughlin104 <dmclaughlin104@gmail.com>
2023-11-16 19:36:38 +0000

    First commit 16.11.23

--- Personal Project/Assets/Scripts/GameManager.cs
+++ Personal Project/Assets/Scripts/GameManager.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class GameManager : MonoBehaviour
 {
 
     public TextMeshProUGUI waveText;
     public TextMeshProUGUI healthText;
+    public TextMeshProUGUI gameOver1;
+    public TextMeshProUGUI gameOver2;
     //private TextMeshProUGUI startMenu;
     private PlayerController playerControllerScript;
-    public int waveNo;
+    private SpawnManager spawnManagerScript;
+
+
+    public bool gameActive;
 
 
     // Start is called before the first frame update
     void Start()
     {
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        spawnManagerScript = GameObject.Find("Spawn Manager").GetComponent<SpawnManager>();
         
     }
 
@@ -30,6 +37,13 @@ public class GameManager : MonoBehaviour
         UpdateHealth(playerControllerScript.healthCount);
 
     }
+
+    //method to start game...
+    public void StartGame()
+    {
+
+    }
+
 
     //method to keep wave text up to date
     public void UpdateWaveText(int pWaveNo)
@@ -46,7 +60,13 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            healthText.text = "You've died - Game Over. You reached wave " + playerControllerScript.waveNumber;
+            healthText.gameObject.SetActive(false);
+            waveText.gameObject.SetActive(false);
+            gameOver2.text = "You reached wave " + playerControllerScript.waveNumber;
+            gameOver1.gameObject.SetActive(true);
+            gameOver2.gameObject.SetActive(true);
+
+
         }
     }
 
